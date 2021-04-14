7755a16e06e2f66ee20ce4b75a694e345b00ed82
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-14 18:44:26 -0300

    Create class Movimentacao

---
+++ Maps/src/maps/Movimentacao.java
@@ -0,0 +1,12 @@
+package maps;
+
+public class Movimentacao {
+
+	private Double valor;
+	private String descricao;
+	
+	public Movimentacao(Double valor, String descricao) {
+		this.valor = valor;
+		this.descricao = descricao;
+	}
+}
