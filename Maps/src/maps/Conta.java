3754dc7f3d03c1d3fe56035f32333fb05103f9db
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-14 20:13:42 -0300

    Add get and set methods to Conta

--- Maps/src/maps/Conta.java
+++ Maps/src/maps/Conta.java
@@ -1,7 +1,38 @@
 package maps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conta {
 	
-	private Double saldo;
+	private double saldo = 0;
+	private List<Movimentacao> movimentacao = new ArrayList<Movimentacao>();
 
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public boolean venda(Double valor) {
+		
+		Movimentacao movimentacaoAtual= new Movimentacao(valor, "Venda");
+		this.saldo += valor;
+		this.movimentacao.add(movimentacaoAtual);
+		return true;
+	}
+	
+	public boolean compra(Double valor) {
+		
+		boolean ok = false;
+		
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			Movimentacao movimentacaoAtual= new Movimentacao(valor, "Compra");
+			this.movimentacao.add(movimentacaoAtual);
+			ok = true;
+		} else {
+			ok = false;
+		}
+		
+		return ok;
+	}
 }
