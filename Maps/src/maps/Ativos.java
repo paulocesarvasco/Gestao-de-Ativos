31a4422b3d5cf2131ebdfcb039033c7295d4673e
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-17 17:59:48 -0300

    Implement CRUD methods to asset register

--- Maps/src/maps/Ativos.java
+++ Maps/src/maps/Ativos.java
@@ -1,28 +1,60 @@
 package maps;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Ativos {
 
-	private String nome;
-	private double preco;
-	private String tipo;
+	private List<Ativo> ativos = new ArrayList<Ativo>();
 	
-	// Ativos nao podem ser criados sem uma verificacaoo previa de seu tipo
-	private Ativos(String nome, double preco, String tipo) {
-		this.nome = nome;
-		this.preco = preco;
-		this.tipo = tipo;
-	}
 	
 	// Método para criar um novo ativo que verifica se seu tipo é valido
-	public Ativos criarAtivo (String nome, double preco, String tipo) {
+	public boolean criarAtivo (String nome, double preco, double quantidade, String tipo) {
+		
+		boolean ok = false;
 		
 		if (Objects.equals(tipo, "RV") || Objects.equals(tipo, "RF") || Objects.equals(tipo, "FUNDO")) {
-			Ativos ativo = new Ativos(nome, preco, tipo);
-			return ativo;
-		} else {
-			return null;
+			Ativo ativo = new Ativo(nome, preco, quantidade, tipo);
+			ativos.add(ativo);
+			ok = true;
 		}
+		return ok;
 	}
+	
+	public Ativo buscaAtivo(String nome) {
+		
+		for (Ativo ativo : this.ativos) {
+			if (Objects.equals(ativo.getNome(), nome)) {
+				return ativo;
+			}
+		}
+		return null;
+	}
+	
+	public boolean removeAtivo(String nome) {
+		
+		Ativo ativo = buscaAtivo(nome);
+		
+		if (ativo == null) {
+			return false;
+		}
+
+		this.ativos.remove(ativo);
+		return true;
+	}
+	
+	public boolean atualizaAtivo(String ativoBuscado, String novoNome, double novoPreco) {
+		
+		Ativo ativo = buscaAtivo(ativoBuscado);
+		
+		if (ativo == null) {
+			return false;
+		}
+		
+		ativo.setNome(novoNome);
+		ativo.setPreco(novoPreco);
+		return true;
+	}
+	
 }
