903071dd4b53f10283aac82293432ceb76a17eac
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-17 20:20:29 -0300

    Change 'movimentacao' class to store aditional infos

--- Maps/src/maps/Movimentacao.java
+++ Maps/src/maps/Movimentacao.java
@@ -3,11 +3,36 @@ package maps;
 // A classe movimentacao contém as informações referentes a cada movimentacao do saldo da conta
 public class Movimentacao {
 
-	private Double valor;
-	private String descricao;
+	private Ativo ativo;
+	private double quantidade;
+	private double valor;
+	private boolean compra;
 	
-	public Movimentacao(Double valor, String descricao) {
+	public Movimentacao(Ativo ativo, double quantidade, double valor) {
+		this.ativo = ativo;
+		this.quantidade = quantidade;
 		this.valor = valor;
-		this.descricao = descricao;
+	}
+	
+	public Ativo getAtivo() {
+		return this.ativo;
+	}
+	
+	public double getQuantidade() {
+		return this.quantidade;
+	}
+	
+	public double getValor() {
+		return this.valor;
+	}
+	
+	// A flag compra em verdadeiro indica que a movimentacao é referente a uma compra,
+	// caso falsa, representa uma venda
+	public void setTipo(boolean compra) {
+		this.compra = compra;
+	}
+	
+	public boolean getTipo() {
+		return this.compra;
 	}
 }
