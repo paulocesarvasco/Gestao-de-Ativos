c9cfbc16893af44bf23a46af8343f189204d23c9
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-17 22:07:46 -0300

    Create class 'Registro'

---
+++ Maps/src/maps/Registro.java
@@ -0,0 +1,21 @@
+package maps;
+
+public class Registro {
+
+	private String nome;
+	private double quantidadeTotal;
+	private double valorMercadoTotal;
+	private double rendimento;
+	private double lucro;
+	private String tipo;
+	
+	public Registro (String nome, String tipo, double quantidade, double valor, double rendimento, double lucro) {
+		this.nome = nome;
+		this.quantidadeTotal = quantidade;
+		this.valorMercadoTotal = valor;
+		this.rendimento = rendimento;
+		this.lucro = lucro;
+		this.tipo = tipo;
+	}
+	
+}
