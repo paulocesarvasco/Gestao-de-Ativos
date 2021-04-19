a4b7cdbbbebae3f6688a62b101d2acfb64a6f00d
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-19 19:59:23 -0300

    Change debug output format

--- Maps/src/maps/Consulta.java
+++ Maps/src/maps/Consulta.java
@@ -9,13 +9,11 @@ public class Consulta {
 
 	// Para cada ativo será gerado um registro que será adicionado a lista de registros
 	// ordenada em ordem alfabética
-	private List<Registro> registros = new ArrayList<Registro>();
+	private static List<Registro> registros = new ArrayList<Registro>();
 	
-	public boolean criarRegistros(List<Movimentacao> movimentacoes, List<Ativo> ativos) {
+	public static boolean criarRegistros(List<Movimentacao> movimentacoes, List<Ativo> ativos) {
 		
-		String nomeAtivo, tipoAtivo;
-		double quantidadeTotal, valorMercado, rendimento, lucro;
-		
+		String nomeAtivo;
 		Registro novoRegistro = null;
 		
 		// ativosRegistrados tem como objetivo apenas auxiliar a marcação de quais ativos já tiveram
@@ -24,22 +22,14 @@ public class Consulta {
 		
 		// Sempre que for requisitado uma lista de registros, a lista atual é apagada
 		// e uma nova lista é criada
-		this.registros.clear();
+		registros.clear();
 		
 		for (Movimentacao movimentacao : movimentacoes) {
-	
 			nomeAtivo = movimentacao.getAtivo().getNome();
-
 			// Se o nome do ativo já estiver presente na lista, passa pro próximo elemento da lista de movimentacoes
 			if (!ativosRegistrados.contains(nomeAtivo)) {
-				quantidadeTotal = contabilizarAtivo(movimentacoes, nomeAtivo);
-				valorMercado = contabilizarValor(ativos, nomeAtivo, quantidadeTotal);
-				rendimento = contabilizarRendimento(ativos, movimentacoes, nomeAtivo);
-				lucro = contabilizarLucro(movimentacoes, nomeAtivo);
-				tipoAtivo = movimentacao.getAtivo().getTipo();
-			
-				novoRegistro = new Registro(nomeAtivo, tipoAtivo, quantidadeTotal, valorMercado, rendimento, lucro);
-				this.registros.add(novoRegistro);
+				novoRegistro = new Registro(movimentacoes, ativos, nomeAtivo, movimentacao.getAtivo().getTipo());
+				registros.add(novoRegistro);
 				ativosRegistrados.add(nomeAtivo);
 			}
 		}
@@ -55,21 +45,21 @@ public class Consulta {
 	}
 	
 	// Método usado apenas para debugar os valores de cada registro
-	public boolean verRegistros() {
+	public static boolean verRegistros() {
 		
 		int index = 1;
 		
-		if (this.registros.isEmpty()) {
+		if (registros.isEmpty()) {
 			return false;
 		}
 
-		System.out.printf("Numero de ativos registrados: %d\n", this.registros.size());
-		for (Registro registro : this.registros) {
+		System.out.printf("Numero de ativos registrados: %d\n", registros.size());
+		for (Registro registro : registros) {
 			System.out.printf("	Ativo %d: %s\n", index, registro.getNome());
-			System.out.printf("		Quantidade total: %.2f\n", registro.getQuantidade());
-			System.out.printf("		Valor de mercado: %.2f\n", registro.getValorMercado());
-			System.out.printf("		Rendimento: %.2f\n", registro.getRendimento());
-			System.out.printf("		Lucro: %.2f\n", registro.getLucro());
+			System.out.printf("		Quantidade total: %f\n", registro.getQuantidade());
+			System.out.printf("		Valor de mercado: %f\n", registro.getValorMercado());
+			System.out.printf("		Rendimento: %f\n", registro.getRendimento());
+			System.out.printf("		Lucro: %f\n", registro.getLucro());
 			System.out.println("");
 			index++;
 		}
