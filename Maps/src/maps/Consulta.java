10e3bcda2a0efce9eea7a892f59cdbae52dfb367
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-19 20:40:56 -0300

    Remove static logic

--- Maps/src/maps/Consulta.java
+++ Maps/src/maps/Consulta.java
@@ -9,9 +9,9 @@ public class Consulta {
 
 	// Para cada ativo será gerado um registro que será adicionado a lista de registros
 	// ordenada em ordem alfabética
-	private static List<Registro> registros = new ArrayList<Registro>();
+	private List<Registro> registros = new ArrayList<Registro>();
 	
-	public static boolean criarRegistros(List<Movimentacao> movimentacoes, List<Ativo> ativos) {
+	public boolean criarRegistros(List<Movimentacao> movimentacoes, List<Ativo> ativos) {
 		
 		String nomeAtivo;
 		Registro novoRegistro = null;
@@ -22,20 +22,20 @@ public class Consulta {
 		
 		// Sempre que for requisitado uma lista de registros, a lista atual é apagada
 		// e uma nova lista é criada
-		registros.clear();
+		this.registros.clear();
 		
 		for (Movimentacao movimentacao : movimentacoes) {
 			nomeAtivo = movimentacao.getAtivo().getNome();
 			// Se o nome do ativo já estiver presente na lista, passa pro próximo elemento da lista de movimentacoes
 			if (!ativosRegistrados.contains(nomeAtivo)) {
 				novoRegistro = new Registro(movimentacoes, ativos, nomeAtivo, movimentacao.getAtivo().getTipo());
-				registros.add(novoRegistro);
+				this.registros.add(novoRegistro);
 				ativosRegistrados.add(nomeAtivo);
 			}
 		}
 		
 		// Ordena a lista de registro de acordo com o nome dos ativos
-		Collections.sort(registros, new Comparator<Registro>() {
+		Collections.sort(this.registros, new Comparator<Registro>() {
 		      public int compare(Registro registro_1, Registro registro_2) {
 		          return registro_1.getNome().compareTo(registro_2.getNome());
 		      }
@@ -45,16 +45,16 @@ public class Consulta {
 	}
 	
 	// Método usado apenas para debugar os valores de cada registro
-	public static boolean verRegistros() {
+	public boolean verRegistros() {
 		
 		int index = 1;
 		
-		if (registros.isEmpty()) {
+		if (this.registros.isEmpty()) {
 			return false;
 		}
 
-		System.out.printf("Numero de ativos registrados: %d\n", registros.size());
-		for (Registro registro : registros) {
+		System.out.printf("Numero de ativos registrados: %d\n", this.registros.size());
+		for (Registro registro : this.registros) {
 			System.out.printf("	Ativo %d: %s\n", index, registro.getNome());
 			System.out.printf("		Quantidade total: %f\n", registro.getQuantidade());
 			System.out.printf("		Valor de mercado: %f\n", registro.getValorMercado());
@@ -65,5 +65,10 @@ public class Consulta {
 		}
 		
 		return true;
+	}
+	
+	// Metodo que fornece a lista contendo todos os registros
+	public List<Registro> getRegistros() {
+		return this.registros;
 	}
 }
